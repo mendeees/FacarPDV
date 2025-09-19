9b747e6f080dc0f3b809342e120c0baed020c7b9
Thales <Thales@DESKTOP-184R5SM>
2025-09-19 19:31:04 -0300

    Adicionar arquivos de projeto.

---
+++ FacarPDV/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FacarPDV.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
