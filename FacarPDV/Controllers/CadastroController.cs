7e37b7002ec24e18033f00b604bc70f1d593d2f9
Thales <Thales@DESKTOP-184R5SM>
2025-09-20 10:38:49 -0300

    Criação de algumas telas

---
+++ FacarPDV/Controllers/CadastroController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FacarPDV.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroProduto()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroUsuario()
+        {
+            return View();
+        }
+    }
+}
---
+++ FacarPDV/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FacarPDV.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string login, string senha)
+        {
+            if (login == "admin" && senha == "123")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Erro = "Usuário ou senha inválidos!";
+            return View();
+        }
+    }
+}
+
